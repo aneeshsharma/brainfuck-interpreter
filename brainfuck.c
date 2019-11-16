3aa1bdd966fe2e7e355c64b8b971d2cd77117475
Anish Sharma <aneeshsharma15@outlook.com>
2019-11-16 13:27:36 +0530

    Fixed the getchar() problem

--- brainfuck.c
+++ brainfuck.c
@@ -92,7 +92,7 @@ int main(int argc, char** argv){
                 #endif
                 break;
             case ',':
-                *ptr = getc(stdout);
+                *ptr = getchar();
                 break;
             case '[':
                 if (*ptr == 0){
